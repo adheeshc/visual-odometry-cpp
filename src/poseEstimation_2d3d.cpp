bdb634aba37de2189e6d11c8c60b87c49324b802
Adheesh <adheeshchat@gmail.com>
2024-03-25 17:25:14 -0700

    updated cmakelists, updated PnP nonlinear optimization using gaussNewton optimization + g2o optimization

--- src/poseEstimation_2d3d.cpp
+++ src/poseEstimation_2d3d.cpp
@@ -2,9 +2,42 @@
 #include <opencv2/opencv.hpp>
 #include <Eigen/Core>
 #include <sophus/se3.hpp>
+#include <g2o/core/base_vertex.h>
+#include <g2o/core/g2o_core_api.h>
+#include <g2o/core/base_unary_edge.h>
+#include <g2o/core/block_solver.h>
+#include <g2o/core/optimization_algorithm_levenberg.h>
+#include <g2o/core/optimization_algorithm_gauss_newton.h>
+#include <g2o/solvers/dense/linear_solver_dense.h>
+#include <chrono>
 
 typedef std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d>> VecVector3d;
 typedef std::vector<Eigen::Vector2d, Eigen::aligned_allocator<Eigen::Vector2d>> VecVector2d;
+
+// Function to convert std::vector<cv::Point3f> to VecVector3d
+VecVector3d convertToEigen3d(const std::vector<cv::Point3f>& points_3d) {
+    VecVector3d eigenPoints;
+    eigenPoints.reserve(points_3d.size()); // Reserve space to avoid reallocation
+
+    // Convert each cv::Point3f to Eigen::Vector3d
+    for (const auto& point : points_3d) {
+        Eigen::Vector3d eigenPoint(point.x, point.y, point.z);
+        eigenPoints.push_back(eigenPoint);
+    }
+    return eigenPoints;
+}
+
+VecVector2d convertToEigen2d(const std::vector<cv::Point2f>& points_2d) {
+    VecVector2d eigenPoints;
+    eigenPoints.reserve(points_2d.size()); // Reserve space to avoid reallocation
+
+    // Convert each cv::Point3f to Eigen::Vector3d
+    for (const auto& point : points_2d) {
+        Eigen::Vector2d eigenPoint(point.x, point.y);
+        eigenPoints.push_back(eigenPoint);
+    }
+    return eigenPoints;
+}
 
 void bundleAdjustmentGaussNewton(
     const VecVector3d& points_3d,
@@ -27,10 +60,177 @@ void bundleAdjustmentGaussNewton(
         cost = 0;
         for (int i = 0;i < points_3d.size();i++) {
             Eigen::Vector3d pc = pose * points_3d[i];
+            double invZ = 1.0 / pc[2];
+            double invZ2 = invZ * invZ;
+            double u = fx * pc[0] / pc[2] + cx;
+            double v = fy * pc[1] / pc[2] + cy;
+            Eigen::Vector2d proj(u, v);
+            Eigen::Vector2d e = points_2d[i] - proj;
+            cost += e.squaredNorm();
+            Eigen::Matrix<double, 2, 6> J;
+            J(0, 0) = -fx * invZ;
+            J(0, 1) = 0;
+            J(0, 2) = fx * pc[0] * invZ2;
+            J(0, 3) = fx * pc[0] * pc[1] * invZ2;
+            J(0, 4) = -(fx + fx * pc[0] * pc[0] * invZ2);
+            J(0, 5) = fx * pc[1] * invZ;
 
+            J(1, 0) = 0;
+            J(1, 1) = -fy * invZ;
+            J(1, 2) = fy * pc[1] * invZ2;
+            J(1, 3) = fy + fy * pc[1] * pc[1] * invZ2;
+            J(1, 4) = -fy * pc[0] * pc[1] * invZ2;
+            J(1, 5) = -fy * pc[0] * invZ;
+
+            H += J.transpose() * J;
+            b += -J.transpose() * e;
+        }
+
+        Vector6d dx;
+        dx = H.ldlt().solve(b);
+
+        if (isnan(dx[0])) {
+            std::cout << "result is nan";
+            break;
+        }
+
+        if (iter > 0 && cost >= lastCost) {
+            //COST INCREASE
+            // std::cout << "cost: " << cost << ", last cost: " << lastCost << std::endl;
+            break;
+        }
+
+        //update estimate
+        pose = Sophus::SE3d::exp(dx) * pose;
+        lastCost = cost;
+
+        // std::cout << "iteration " << iter << " cost=" << std::cout.precision(12) << cost << std::endl;
+        if (dx.norm() < 1e-6) {//convergenece    
+            break;
         }
     }
+    std::cout << "Pose Estimate by GaussNewton: \n" << pose.matrix() << std::endl;
+}
 
+class VertexPose : public g2o::BaseVertex<6, Sophus::SE3d> {
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+
+    //override reset function
+    virtual void setToOriginImpl() override {
+        _estimate = Sophus::SE3d();
+    }
+
+    //override plus operator
+    virtual void oplusImpl(const double* update) override {
+        Eigen::Matrix<double, 6, 1> updateEigen;
+        updateEigen << update[0], update[1], update[2], update[3], update[4], update[5];
+        _estimate = Sophus::SE3d::exp(updateEigen) * _estimate;
+    }
+
+    //dummy read/write function
+    virtual bool read(std::istream& in) {};
+    virtual bool write(std::ostream& out) const {};
+};
+
+class EdgeProjection : public g2o::BaseUnaryEdge<2, Eigen::Vector2d, VertexPose> {
+private:
+    Eigen::Vector3d _pos3d;
+    Eigen::Matrix3d _K;
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+    EdgeProjection(const Eigen::Vector3d& pos, const Eigen::Matrix3d& K) : _pos3d(pos), _K(K) {};
+
+    //define error term computation
+    virtual void computeError() override {
+        const VertexPose* v = static_cast<VertexPose*>(_vertices[0]);
+        Sophus::SE3d T = v->estimate();
+        Eigen::Vector3d posPixel = _K * (T * _pos3d);
+        posPixel /= posPixel[2];
+        _error = _measurement - posPixel.head<2>();
+    }
+
+    //Jacobian
+    virtual void linearizeOplus() override {
+        const VertexPose* v = static_cast<VertexPose*>(_vertices[0]);
+        Sophus::SE3d T = v->estimate();
+        Eigen::Vector3d posCam = T * _pos3d;
+        double fx = _K(0, 0);
+        double fy = _K(1, 1);
+        double cx = _K(0, 2);
+        double cy = _K(1, 2);
+        double X = posCam[0];
+        double Y = posCam[1];
+        double Z = posCam[2];
+        double Z2 = Z * Z;
+        // _jacobianOplusXi << -fx / Z, 0, fx* X / Z2, fx* X* Y / Z2, -fx - fx * X * X / Z2, fx* Y / Z,
+            // 0, -fy / Z, fy* Y / Z2, fy + fy * Y * Y / Z2, -fy * X * Y / Z2, -fy * X / Z;
+            _jacobianOplusXi(0, 0) = -fx / Z;
+            _jacobianOplusXi(0, 1) = 0;
+            _jacobianOplusXi(0, 2) = fx * X / Z2;
+            _jacobianOplusXi(0, 3) = fx * X * Y / Z2;
+            _jacobianOplusXi(0, 4) = -fx - fx * X * X / Z2;
+            _jacobianOplusXi(0, 5) = fx * Y / Z;
+
+            _jacobianOplusXi(1, 0) = 0;
+            _jacobianOplusXi(1, 1) = -fy / Z;
+            _jacobianOplusXi(1, 2) = fy * Y / Z2;
+            _jacobianOplusXi(1, 3) = fy + fy * Y * Y / Z2;
+            _jacobianOplusXi(1, 4) = -fy * X * Y / Z2;
+            _jacobianOplusXi(1, 5) = -fy * X / Z;
+    }
+
+    //dummy read/write function
+    virtual bool read(std::istream& in) {};
+    virtual bool write(std::ostream& out) const {};
+};
+
+void bundleAdjustmentg2o(
+    const VecVector3d& points_3d,
+    const VecVector2d& points_2d,
+    const cv::Mat& K,
+    Sophus::SE3d& pose) {
+
+    typedef g2o::BlockSolver<g2o::BlockSolverTraits<6, 3>> BlockSolverType;  //pose -6, landmark(x,y,z) - 3
+    typedef g2o::LinearSolverDense<BlockSolverType::PoseMatrixType> LinearSolverType;
+    auto solver = new g2o::OptimizationAlgorithmGaussNewton(g2o::make_unique<BlockSolverType>(g2o::make_unique<LinearSolverType>()));
+    // auto solver = new g2o::OptimizationAlgorithmLevenberg(g2o::make_unique<BlockSolverType>(g2o::make_unique<LinearSolverType>()));
+
+    g2o::SparseOptimizer optimizer;
+    optimizer.setAlgorithm(solver);
+    optimizer.setVerbose(false);
+
+    //vertex
+    VertexPose* vertexPose = new VertexPose();
+    vertexPose->setId(0);
+    vertexPose->setEstimate(Sophus::SE3d());
+    optimizer.addVertex(vertexPose);
+
+    //convert K
+    Eigen::Matrix3d KEigen;
+    KEigen <<
+        K.at<double>(0, 0), K.at<double>(0, 1), K.at<double>(0, 2),
+        K.at<double>(1, 0), K.at<double>(1, 1), K.at<double>(1, 2),
+        K.at<double>(2, 0), K.at<double>(2, 1), K.at<double>(2, 2);
+
+    // //edges
+    int index = 1;
+    for (size_t i = 0; i < points_2d.size();i++) {
+        auto p2d = points_2d[i];
+        auto p3d = points_3d[i];
+        EdgeProjection* edge = new EdgeProjection(p3d, KEigen);
+        edge->setId(index);
+        edge->setVertex(0, vertexPose);
+        edge->setMeasurement(p2d);
+        edge->setInformation(Eigen::Matrix2d::Identity());
+        optimizer.addEdge(edge);
+        index++;
+    }
+    optimizer.initializeOptimization();
+    optimizer.optimize(10);
+
+    pose = vertexPose->estimate();
+    std::cout << "Pose Estimate by g2o: \n" << pose.matrix() << std::endl;
 }
 
 cv::Point2d pixel2cam(const cv::Point2d& p, cv::Mat& K) {
@@ -117,8 +317,37 @@ int main() {
     cv::Mat R;
     cv::Rodrigues(r, R); //r is rotation vector, R is rotation matrix
 
-    std::cout << "R: " << R << std::endl;
-    std::cout << "t: " << t << std::endl;
+    // std::cout << "R: " << R << std::endl;
+    // std::cout << "t: " << t << std::endl;
+    cv::Mat T = cv::Mat::eye(3, 4, CV_64F);
+    T.at<double>(0, 0) = R.at<double>(0, 0);
+    T.at<double>(0, 1) = R.at<double>(0, 1);
+    T.at<double>(0, 2) = R.at<double>(0, 2);
+    T.at<double>(1, 0) = R.at<double>(1, 0);
+    T.at<double>(1, 1) = R.at<double>(1, 1);
+    T.at<double>(1, 2) = R.at<double>(1, 2);
+    T.at<double>(2, 0) = R.at<double>(2, 0);
+    T.at<double>(2, 1) = R.at<double>(2, 1);
+    T.at<double>(2, 2) = R.at<double>(2, 2);
+
+    T.at<double>(0, 3) = t.at<double>(0, 0);
+    T.at<double>(1, 3) = t.at<double>(1, 0);
+    T.at<double>(2, 3) = t.at<double>(2, 0);
+
+    std::cout << "Original Estimated Pose: " << T << std::endl;
+
+    Sophus::SE3d pose;
+    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+    bundleAdjustmentGaussNewton(convertToEigen3d(points_3d), convertToEigen2d(points_2d), K, pose);
+    std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+    std::chrono::duration<double> timeTaken = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    std::cout << "Time Taken by GaussNewton: " << timeTaken.count() << " seconds" << std::endl;
+
+    t1 = std::chrono::steady_clock::now();
+    bundleAdjustmentg2o(convertToEigen3d(points_3d), convertToEigen2d(points_2d), K, pose);
+    t2 = std::chrono::steady_clock::now();
+    timeTaken = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    std::cout << "Time Taken by g2o: " << timeTaken.count() << " seconds" << std::endl;
 
     return 0;
 }
